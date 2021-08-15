863b16c0063de59864934d36d3c03fd8e88935d3
Oidamoida <btduong@pm.me>
2021-08-15 15:35:12 +0200

    remove marks

--- vegenere.py
+++ vegenere.py
@@ -29,7 +29,19 @@ rot_list = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,
 'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,
 'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
 
+marks = [' ',',','.','?','!']
+
 keylen = len(key)
+messlen = len(message)
+
+#remove marks
+s = 0
+for y in range(0, messlen):
+  if message[y] in marks:
+    message = message[:y] + message[y+1:]
+    message = message + "e"
+    s = s + 1
+message = message[:-s]
 messlen = len(message)
 
 #Convert letters to digits
@@ -40,8 +52,6 @@ for a in range(0, keylen):
 messlist = []
 for b in range(0, messlen):
   messlist.append(rot_list[message[b]])
-
-
 
 #How often will the key be used
 fest = keylist
@@ -83,9 +93,6 @@ if answer == 'e':
   for i in Cryptmess:
     res_string=res_string+i
   print(res_string)
-
-
- 
 
 #Decryption
 elif answer == 'd':
